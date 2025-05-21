39b81621e9e043cf9e8707b9eae622bbb9fe1848
unknown <christianmolicki@gmail.com>
2025-05-21 14:59:08 +0200

    fix getting guest token

--- twitterdownloader.py
+++ twitterdownloader.py
@@ -366,10 +366,30 @@ class TwitterDownloader():
             info["nsfw"] = True
         return info
     async def _get_guest_token(self):
-        
-        async with self.session.post('https://api.x.com/1.1/guest/activate.json', headers=self.headers, ) as r:
-            a = await r.json()
-            return a['guest_token']
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'accept-language': 'en-US,en;q=0.7',
+            'priority': 'u=0, i',
+            'sec-ch-ua': '"Chromium";v="136", "Brave";v="136", "Not.A/Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'sec-gpc': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+        }
+        pattern = r"document\.cookie=\"gt=(\d+);"
+        async with self.session.get("https://x.com", headers=headers) as r:
+            while True:
+                chunk = await r.content.read(1024)
+                if not chunk:
+                    break
+                if (match := re.search(pattern, chunk.decode("utf-8"))):
+                    return match.group(1)
+        return None
     async def _get_api_url(self):
         
         if not hasattr(self, "jslink"):
