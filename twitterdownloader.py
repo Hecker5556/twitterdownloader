e4d9cfcdc26db6a5c7fd75628e79be03b274ec24
unknown <christianmolicki@gmail.com>
2026-06-24 21:11:51 +0200

    provide self.headers in grok __init__

--- twitterdownloader.py
+++ twitterdownloader.py
@@ -860,6 +860,24 @@ class Grok(TwitterDownloader):
         self.img_gen_count = img_gen_count
         super().__init__(*args)
         self.data = self.example_data(model, img_gen_count)
+        self.headers = {
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.7',
+            'content-type': 'application/json',
+            'origin': 'https://x.com',
+            'priority': 'u=1, i',
+            'referer': 'https://x.com/',
+            'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Brave";v="128"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+            'x-twitter-active-user': 'yes',
+            'x-twitter-client-language': 'en',
+        }
     async def __aenter__(self):
         self.started = False
         return self
